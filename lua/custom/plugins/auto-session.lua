a38e8aeeb76c9d772776d66ff064cb4ed7cb4327
Nikolai Smirnow <nik.smirnow@wyyerd.com>
2024-02-06 19:48:32 -0700

    comment out auto-session.lua - don't know if it's doing anything...

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -1,22 +1,23 @@
-return {
-  'rmagatti/auto-session',
-  config = function()
-    require("auto-session").setup {
-      log_level = "error",
-      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-      post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
-        require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
-      end,
-      auto_session_enable_last_session = true,
-      auto_session_enabled = true,
-      auto_session_create_enabled = true,
-      auto_save_enabled = true,
-      auto_restore_enabled = true,
-      auto_session_use_git_branch = true,
-    }
-    require('lualine').setup {
-      sections = { lualine_c = { require('auto-session.lib').current_session_name }}
-    }
-  end,
-}
+-- return {
+--   'rmagatti/auto-session',
+--   config = function()
+--     require("auto-session").setup {
+--       log_level = "error",
+--       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+--       post_cwd_changed_hook = function() -- example refreshing the lualine status line _after_ the cwd changes
+--         require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+--       end,
+--       auto_session_enable_last_session = true,
+--       auto_session_enabled = true,
+--       auto_session_create_enabled = true,
+--       auto_save_enabled = true,
+--       auto_restore_enabled = true,
+--       auto_session_use_git_branch = true,
+--     }
+--     require('lualine').setup {
+--       sections = { lualine_c = { require('auto-session.lib').current_session_name }}
+--     }
+--   end,
+-- }
+return {}
 
