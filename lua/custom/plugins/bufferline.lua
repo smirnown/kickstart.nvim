5f8232921293b8ff02f2908a35d3309e0aeaae61
Nikolai Smirnow <nik.smirnow@wyyerd.com>
2024-02-06 19:43:08 -0700

    Lazy sync + bufferline.nvim (I commented it out for now, because I don't know if I like it)

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+-- return {
+--   "akinsho/bufferline.nvim",
+--   version = "4.5.0",
+--   dependencies = "nvim-tree/nvim-web-devicons",
+--   config = function()
+--     vim.opt.termguicolors = true,
+--     require("bufferline").setup{}
+--   end,
+-- }
+return {}
+
