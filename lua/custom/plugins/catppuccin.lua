62df1fb057f2df991666d81c0d9e9acc6b7c9882
Nikolai Smirnow <nik.smirnow@wyyerd.com>
2024-02-14 11:10:34 -0700

    adjust catpuccin settings

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,19 +1,18 @@
 return {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000,
     config = function()
         require("catppuccin").setup({
-            flavour = "frappe", -- latte, frappe, macchiato, mocha
-            -- background = { -- :h background
-            --     light = "latte",
-            --     dark = "mocha",
-            -- },
-            transparent_background = true, -- disables setting the background color.
+            flavour = "macchiato", -- latte, frappe, macchiato, mocha
+            background = { -- :h background
+                light = "latte",
+                dark = "mocha",
+            },
+            transparent_background = false, -- disables setting the background color.
             show_end_of_buffer = false, -- shows the '~' characters after buffers
             term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
             dim_inactive = {
-                enabled = false, -- dims the background color of inactive window
+                enabled = true, -- dims the background color of inactive window
                 shade = "dark",
                 percentage = 0.15, -- percentage of the shade to apply to the inactive window
             },
