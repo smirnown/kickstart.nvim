18df745f538e298465fdae6778f58935e8961e9b
Nikolai Smirnow <nik.smirnow@wyyerd.com>
2024-03-18 16:06:13 -0600

    nvim tmux navigation

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -12,10 +12,10 @@ return {
     vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
     vim.keymap.set("n", "<C-o>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-i>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-z>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-x>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-a>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -8,7 +8,25 @@ return {
     require("oil").setup({
       view_options = {
         show_hidden = true,
-      }
+      },
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-s>"] = false,
+        ["<C-h>"] = false,
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+      },
     })
   end,
 }
