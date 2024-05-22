943a4159e6f77f49ca18c612fd19eba87626e322
Nikolai Smirnow <nik.smirnow@wyyerd.com>
2024-05-22 11:53:37 -0600

    configure formatters for other filetypes

--- init.lua
+++ init.lua
@@ -133,7 +133,12 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        javascript = { { 'prettierd', 'prettier' } },
+        javascript = { 'prettier' },
+        vue = { 'prettier' },
+        json = { 'prettier' },
+        typescript = { 'prettier' },
+        scss = { 'trim_whitespace' },
+        ['_'] = { 'trim_whitespace' },
       },
     },
   },
